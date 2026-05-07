464fe94c33dead991dbb917032b563279e142e95
tbandrowsky <todd.bandrowsky@gmail.com>
2026-05-07 12:26:34 -0500

    Add project files.

---
+++ County.cs
@@ -0,0 +1,17 @@
+﻿namespace libfreedomsharp
+{
+    public class County
+    {
+
+        public string StateName { get; set; }
+        public string CountyName { get; set; }
+        public string Decision { get; set; }
+
+        public County(string stateName, string countyName, string firstDecision, string finalDecision)
+        {
+            StateName = stateName;
+            CountyName = countyName;
+            Decision = finalDecision;
+        }
+    }
+}
